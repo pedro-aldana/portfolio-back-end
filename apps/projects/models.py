ca183fb4d6ce0faa5191e3a5d762245060413c7f
pedro-aldana <pedroaldana987@gmail.com>
2023-09-13 10:36:04 -0500

    arreglo query

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from ckeditor.fields import RichTextField
 import uuid
 # Create your models here.
@@ -9,6 +10,12 @@ def project_thumbnail_directory(instance,filename):
 
 class Project(models.Model):
     
+    class PostObject(models.Manager):
+        def get_queryset(self) -> QuerySet:
+            return super().get_queryset().filter(status='title')
+        
+        
+    
     title = models.CharField(max_length=255,blank=True,null=True)
     slug = models.SlugField(max_length=255,unique=True, default=uuid.uuid4)
     description =   models.TextField(max_length=255,blank=True, null=True)
@@ -16,6 +23,9 @@ class Project(models.Model):
     language = models.CharField(max_length=255,blank=True,null=True)
     url = models.CharField(max_length=600,blank=True,null=True)
     thumbnail =     models.ImageField(upload_to=project_thumbnail_directory,max_length=500,blank=True, null=True)
+    
+    class Meta:
+        ordering = ['title']
     
     
     
